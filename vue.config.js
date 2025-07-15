cf15949b4e6ec30a036ac5afb78bfb119dcaa298
TiAmo1029 <139143296+TiAmo1029@users.noreply.github.com>
2025-07-15 23:31:02 +0800

    vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,6 @@ module.exports = defineConfig({
   // --- 核心配置在这里 ---
   // publicPath 决定了应用在打包后，资源文件（如JS, CSS）的引用路径
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/city-explorer/' // 生产环境下，我们把它部署在 /city-explorer/ 子目录下
+    ? '/City_Explorer/' // 生产环境下，我们把它部署在 /city-explorer/ 子目录下
     : '/'             // 开发环境下，我们还是在根目录
 })
